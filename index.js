f46dbbffe0817d26eacc81b5f7d3ae07ee7e7c38
JAYAPRASAD B <127640948+Jayaprasad78@users.noreply.github.com>
2024-03-31 08:24:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,41 +4,29 @@ const app = express();
 const cors = require('cors');
 app.use(cookieParser());
 const dotenv = require('dotenv');
-dotenv.config({path:'./config.env'});
+dotenv.config({ path: './config.env' });
 app.use(express.static('public'));
 
-app.use(cors(
-    {
-        origin:'https://angularback1.vercel.app',
-        methods: ["POST", "GET"],
-        credentials: true
-    }
-));
+app.use(
+  cors({
+    origin: 'https://angularback1.vercel.app',
+    methods: ["POST", "GET"],
+    credentials: true
+  })
+);
 
-const port=process.env.PORT;
+const port = process.env.PORT;
 
 require('./db/conn');
 require('./userschema/userschema');
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-res.send("hi hello")
+app.get('/', (req, res) => {
+  res.send("hi hello");
 });
+
 app.use(require('./router/auth'));
 
-
-
-
-// app.get('/about',(req,res)=>{
-//     res.send('About page');
-// });
-
-
-
-
-
-
-
-app.listen(port,()=>{
-    console.log(`server listening on port ${port} `);
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
 });
